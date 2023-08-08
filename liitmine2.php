26de4ce1a29ceb0ca4014485c8ae52eaae943096
AstrayJ <jarljustus.hellat@real.edu.ee>
2023-08-08 15:01:59 +0300

    Added an unfinished math test

---
+++ liitmine2.php
@@ -0,0 +1,32 @@
+<?php
+$a = 0;
+
+while ($a <= 5){
+    $x = random_int(0, 10);
+    $y = random_int(0, 10);
+    ?>
+    <form action="liitmine2.php" method="post">
+    <?php echo $x . "+" .$y; ?>: <input type="number" name='name'><br>
+    <input type="submit" name='nupp'>
+    </form> 
+    <script>
+      $(document).on("keypress", "form", function(event) {
+         if (event.keyCode === 13) {
+            event.preventDefault();
+            $(this).submit();
+         }
+      });
+   </script>
+
+<?php 
+    if (isset($_POST['nupp'])){
+        unset($_POST['nupp']);
+        continue;
+    } else{
+        die();
+    }
+    if((int)$_POST["name"] === $x + $y){
+        $a++;
+    }
+}
+?>
