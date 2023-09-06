2d78f0949f49efaed615a423515ba33d6666cd70
AstrayJ <jarljustus.hellat@real.edu.ee>
2023-09-06 22:31:45 +0300

    Array generator No.1

--- math/index1.php
+++ math/index1.php
@@ -11,32 +11,36 @@ if(!isset($_SESSION['score'])  && !isset($_SESSION['level'])){
 
 
 // Generate random numbers
-$x = random_int(0,10);
-$y = random_int(0,10);
+$x = random_int(0,9);
+$y = random_int(1,10);
+$q = 0;
+$z = array_fill(1, 15 + $q, $x . '+'. $y = $x+$y);/* plaanin saata info arrayga */
+    
 
-// Stopwatch 
-function startTime(){
-    $sec = 0;
-    $min = 0;
-    do {
-        echo $min. ':'. $sec;
-        sleep(1);
-        $sec ++;
-        if($sec === 60){
-            $min++;
-        }
 
-    }while($_SESSION['score'] <= 15);
-    if($_SESSION['score'] === 15){
-        echo "Sinu aeg oli ".$min. ':'. $sec;
-    }
-}
+// // Stopwatch 
+// function startTime(){
+//     $sec = 0;
+//     $min = 0;
+//     do {
+//         echo $min. ':'. $sec;
+//         sleep(1);
+//         $sec ++;
+//         if($sec === 60){
+//             $min++;
+//         }
+
+//     }while($_SESSION['score'] <= 15);
+//     if($_SESSION['score'] === 15){
+//         echo "Sinu aeg oli ".$min. ':'. $sec;
+//     }
+// }
 
 // Function to verify the answer posted
 function verifyAnswer(){
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        // Reset button click, reset score and exit function
+        // Reset button click, reset score & level and exit function
         if(isset($_POST["reset"])){
             $_SESSION["score"] = 0;
             $_SESSION["level"] = 1;
@@ -50,9 +54,12 @@ function verifyAnswer(){
         if ((int)$answer === $_POST["x"] + $_POST["y"]) {
             $_SESSION["score"] += 1;
             echo "Õige vastus";
+            
         }else{
             echo "Vale vastus, õige olnuks " . $_POST["x"] + $_POST["y"];
+            unset($_POST['answer']);
         }
+        
         
         // Winning condition
         if($_SESSION['score'] > 15){
@@ -62,30 +69,64 @@ function verifyAnswer(){
     }
 }
 
+//Muliplier for smoother transitions \\\ NEEDS WORK
+function multyplier($var1 , $var2, $mult, $Max){
+    $var2 = round($var2 * $mult);
+    $var1 = round($var1 * $mult);
+    $mult =+ 0.3;
+    if(($var2+$var1) > $Max){
+        $var2+$var1 = 0.7 * ($var2+$var1);
+    };
+    
+}
+
 //ascending level system
-verifyAnswer();
+if($_SESSION["score"] >= 0){
+    $_SESSION["level"] = 1;
+    $x = random_int(0 ,9);
+    $y = random_int(1,10);
+    multyplier($x, $y, 0.5, 10);
+    
+}
 if($_SESSION["score"] >= 5){
     $_SESSION["level"] = 2;
-    $x = random_int(10,100);
-    $y = random_int(10,100);
+    $x = random_int(10 ,99);
+    $y = random_int(11,100);
+    multyplier($x, $y, 0.5, 100);
 }
 if($_SESSION["score"] >= 9){
     $_SESSION["level"] = 3;
-    $x = random_int(100,1000);
-    $y = random_int(100,1000);
+    $x = random_int(100,999);
+    $y = random_int(101,1000);
+    multyplier($x, $y, 0.5, 1000);
 }
 if($_SESSION["score"] >= 12){
     $_SESSION["level"] = 4;
-    $x = random_int(1000,10000);
-    $y = random_int(1000,10000);
+    $x = random_int(1000,9999);
+    $y = random_int(1001,10000);
+    multyplier($x, $y, 0.5, 10000);
 }
+verifyAnswer();
 
+// Array generator
+function array_Gen($var1, $var2){
+    $loend = [] ;
+    $pop = 0 ;
+do {
+    $loend[$var1 . '+' . $var2] = $var1 + $var2;
+    $pop ++;
+}while($pop <=15);
+    var_dump($loend);
+};
 
+array_Gen($x, $y);
 ?>
 
 <form method="post">
+<button onclick="window.location.href='http://localhost:3000/korrutamine.php';">Korrutamine</button><button onclick="window.location.href='http://localhost:3000/jagamine.php';">Jagamine</button><button onclick="window.location.href='http://localhost:3000/lahutamine.php';">Lahutamine</button><br>
 <p style="display: inline;">Skoor: <?php echo $_SESSION["score"] ?></p> <button style="display: inline-block;" name="reset">Nulli</button><br>
 <p style="display: inline;">Tase: <?php echo $_SESSION["level"] ?></p> 
+
 
 </form>
 
