2b9368a30d021e67bbc8ae8767a37a0f4269aad0
AstrayJ <jarl.justus.hellat@gmail.com>
2023-11-09 21:44:08 +0200

    Small adjustments to Controller, deleted old files

--- math/korrutamine.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION['level'] = 1;
-// Initial value for score and level
-if(!isset($_SESSION['score'])  && !isset($_SESSION['level'])){
-    $_SESSION['score'] = 0;
-    $_SESSION['level'] = 1;
-}
