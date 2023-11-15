929efd6a3504ac6c28063c61390a336d6387db26
AstrayJ <131436862+AstrayJ@users.noreply.github.com>
2023-11-15 09:54:26 +0200

    Update Controller.php
    "Pre changes"

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -28,7 +28,15 @@ class GameController extends Controller
             $mult =+ 0.2;
             if(($var2+$var1) > $Max){
                 $var2+$var1 = 0.7 * ($var2+$var1);
-            };
+            }
+        }
+        function multiPlier($var1 , $var2, $mult, $Max){
+            $var2 = round($var2 * $mult);
+            $var1 = round($var1 * $mult);
+            $mult =+ 0.05;
+            if(($var2+$var1) > $Max){
+                $var2+$var1 = 0.2 * ($var2+$var1);
+            }
             
         }
 
