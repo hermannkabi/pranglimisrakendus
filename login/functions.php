b519f1b12418958d1d671e21cb02b42c1afeedc7
AstrayJ <jarl.justus.hellat@gmail.com>
2024-01-11 23:19:23 +0200

    Small changes to database

--- login/functions.php
+++ login/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function emptyInputSignup($name, $famname, $email, $class, $pwd, $pwdRepeat) {
-    return empty($name) || empty($email) || empty($class) || empty($pwd) || empty($pwdRepeat);
+    return empty($name) || empty($famname) || empty($email) || empty($class) || empty($pwd) || empty($pwdRepeat);
 };
 
 function invalidname($name) {
@@ -36,8 +36,8 @@ function  nameExists($conn, $name, $email) {
     mysqli_stmt_close($stmt);
 };
 
-function  createUser($conn, $name, $email, $class, $pwd) {
-    $sql = "INSERT INTO users (usersName, usersClass, usersEmail, usersPwd) VALUES (?, ?, ?, ?);";
+function  createUser($conn, $name, $famname, $email, $class, $pwd) {
+    $sql = "INSERT INTO users (usersName, usersFamName, usersClass, usersEmail, usersPwd) VALUES (?, ?, ?, ?);";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
         echo 'Error';
@@ -46,7 +46,7 @@ function  createUser($conn, $name, $email, $class, $pwd) {
 
     $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
 
-    mysqli_stmt_bind_param($stmt, "ssss",  $name, $class, $email, $hashedPwd);
+    mysqli_stmt_bind_param($stmt, "sssss",  $name, $famname, $class, $email, $hashedPwd);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
     echo 'Success';
--- login/signup.php
+++ login/signup.php
@@ -1,2 +1,20 @@
 <?php
 
+if (isset($_POST['registration'])) {
+
+    $name = $_POST['name'];
+    $famname = $_POST['famname'];
+    $email = $_POST['email'];
+    $class = $_POST['class'];
+    $adminpsw = $_POST['adminpsw'];
+    $pwd = $_POST['pwd'];
+    $pwdRepeat = $_POST['pwdrepeat'];
+
+    require_once 'database.php';
+    require_once 'functions.php';
+
+    if (emptyInputSignup($name, $famname, $email, $class, $pwd, $pwdRepeat) != false) {
+        echo 'Error'; // Add more inf
+        exit();
+    };
+}
