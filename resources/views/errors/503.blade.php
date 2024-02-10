0824f39a7a6c0dd38cd6478a41f05018f26e7cdc
hermannkabi <hermann.kaebi@gmail.com>
2024-02-10 18:50:13 +0200

    New error pages, cosmetic changes

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,23 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>404 - Ei leitud | Pranglimisrakendus</title>
+    <title>503 - Hooldus | Pranglimisrakendus</title>
     <link rel="stylesheet" href="{{ URL::asset('css/variables.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/master.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/404.css') }}">
 </head>
 <body>
     <div class="error-section">
-        <p class="operation">
-            @php
-                $ops = ["202+202=", "2·202=", "400+4=", "4040:10=", "101·4=", "444-44+4="];
-
-                echo $ops[array_rand($ops)];
-            @endphp
-        </p>
-        <h1 class="code">404</h1>
-        <p>SELLIST LEHEKÜLGE EI LEITUD</p>
-
+        <p class="operation">VIGA</p>
+        <h1 class="code">503</h1>
+        <p>SERVER ON HOOLDUSES</p>
     </div>
 
     <div class="symbs">
