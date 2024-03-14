d70db188c75e8b0697a08b2d7f56758a937fa504
Hermann <hermann.kaebi@gmail.com>
2024-03-14 18:08:24 +0200

    Fix auth bug in routes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-// See on väga halb kood, lihtsalt selleks, et lehed töötaks praegu
-
 Route::get('/', function () {
     if(Auth::check()){
         return redirect()->route("dashboard");
@@ -23,7 +21,7 @@ Route::get('/profile', function () {
     return Inertia::render("Profile/ProfilePage");
 })->name("profilePage")->middleware('auth');
 
-Route::controller(App\Http\Controllers\ProfileController::class)->group(function() {
+Route::controller(App\Http\Controllers\ProfileController::class)->middleware(['throttle:6,1', 'auth'])->group(function() {
     Route::get('/profile', "show")->name("profilePage");
 
     //Route::get('/checkstreak', "checkStreak")->name("checkstreak");
@@ -31,11 +29,11 @@ Route::controller(App\Http\Controllers\ProfileController::class)->group(function
     Route::post('/profile/settings/edit', "settings")->name("settingsAdd");
     Route::post('/profile/avatar', 'changeProfilePicture')->name('changeProfilePicture');
 
-}) -> middleware(['throttle:6,1', 'auth']);
+});
 
 
 //Login and registration
-Route::controller(App\Http\Controllers\Auth\LoginRegisterController::class)->group(function() {
+Route::controller(App\Http\Controllers\Auth\LoginRegisterController::class)->middleware('throttle:6,1')->group(function() {
     Route::get('/register', 'register')->name('register');
     Route::get('/register/google', 'registerGoogle')->name('registerGoogle');
 
@@ -48,38 +46,38 @@ Route::controller(App\Http\Controllers\Auth\LoginRegisterController::class)->gro
 
     Route::get('/dashboard', 'dashboard')->name('dashboard');
     Route::get('/logout', 'logout')->name('logout');
-}) -> middleware('throttle:6,1');
+});
 
 //Google login
-Route::controller(App\Http\Controllers\GoogleLoginController::class)->group(function() {
+Route::controller(App\Http\Controllers\GoogleLoginController::class)->middleware('throttle:6,1')->group(function() {
     Route::get('/google/redirect', 'redirectToGoogle')->name('google.redirect');
     Route::get('/google/callback', 'handleGoogleCallback')->name('google.callback');
-})->middleware('throttle:6,1');
+});
 
 //Email verification
-Route::controller(App\Http\Controllers\AuthVerificationController::class)->group(function() {
+Route::controller(App\Http\Controllers\AuthVerificationController::class)->middleware(['auth','throttle:6,1'])->group(function() {
     Route::get('/email/verify', 'notice')->name('verification.notice');
     Route::get('/email/verify/{id}/{hash}', 'verify')->name('verification.verify');
     Route::post('/email/resend', 'resend')->name('verification.resend');
-})->middleware(['auth','throttle:6,1']);
+});
 
 //Password reset
-Route::controller(App\Http\Controllers\Auth\PasswordResetLinkController::class)->group(function() {
+Route::controller(App\Http\Controllers\Auth\PasswordResetLinkController::class)->middleware(['guest', 'throttle:4,1'])->group(function() {
     Route::get('/forgot-password', 'create')->name('password.request');
     Route::get('/forgot-password', 'store')->name('password.email');
-})->middleware(['guest', 'throttle:4,1']);
+});
 
 //Password reset form
-Route::controller(App\Http\Controllers\Auth\NewPasswordController::class)->group(function() {
+Route::controller(App\Http\Controllers\Auth\NewPasswordController::class)->middleware(['guest', 'throttle:4,1'])->group(function() {
     Route::get('/reset-password/{token}', 'create')->name('password.reset');
     Route::get('/reset-password', 'store')->name('password.update');
-})->middleware(['guest', 'throttle:4,1']);
+});
 
 //User information
-Route::controller(App\Http\Controllers\ProfileController::class)->group(function() {
+Route::controller(App\Http\Controllers\ProfileController::class)->middleware('auth')->group(function() {
     Route::post('/user/settings', 'settings')->name('user-settings');
     Route::post('/delete-user',  'destroy')->name('delete-user');
-})->middleware('auth');
+});
 
 
 Route::get('/ui', function () {
@@ -101,16 +99,16 @@ Route::get("/game/{level}/{mis}/{aeg}/{tüüp}", function ($level, $mis, $aeg, $
 })->name("gameNew")->middleware(['auth', 'throttle: 6,1']);
 
 //Game data
-Route::controller(App\Http\Controllers\GameController::class)->group(function() {
+Route::controller(App\Http\Controllers\GameController::class)->middleware(["auth"])->group(function() {
     Route::post('/game/store', 'store')->name('gameStore');
     Route::post('/game/update', 'update')->name('gameUpdate');
     Route::get('/game/history', 'show')->name('gameHistory');
     Route::post('/game/scoreboard', 'index')->name('gameScoreboard');
     Route::get("game/{id}/details", "gameDetails");
-})->middleware('auth');
+});
 
 //Classroom data
-Route::controller(App\Http\Controllers\ClassController::class)->group(function (){
+Route::controller(App\Http\Controllers\ClassController::class)->middleware(["auth"])->group(function (){
     //Route::get('/classroom/edit/{id}', 'edit')->name('classEdit');
     Route::post('/classroom/search', 'index')->name('classSearch');
     Route::get('/classroom/view/{id}', 'show')->name('classShow');
@@ -118,7 +116,7 @@ Route::controller(App\Http\Controllers\ClassController::class)->group(function (
     Route::post('/classroom/store', 'store')->name('classStore')->middleware(['role:teacher', 'throttle: 4,1']);
     Route::post('/classroom/delete', 'destroy')->name('classDelete')->middleware('role:teacher');
     Route::post('/classroom/remove}', 'destroy')->name('classRemove')->middleware('role:teacher');
-})->middleware('auth');
+});
 
 Route::get('/dashboard/old', function (){
     return Inertia::render("Dashboard/OldDashboardPage");
