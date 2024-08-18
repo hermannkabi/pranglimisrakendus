1fdeb0a86930492f6ada402b7829a4a68c5f9b3b
Hermann <hermann.kaebi@gmail.com>
2024-08-18 16:52:57 +0300

    Better time handling

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -22,19 +22,23 @@
             return formattedTime;
         }
 
-        function manageServerClock(){
-            document.querySelector("#clock").innerText = timeStampToTime(time);
-        }
+        var time = parseInt("{{ $server_time }}");
+        const localTime = Math.floor(Date.now() / 1000);
 
-        var time = "{{ $server_time }}";
+        function manageServerClock(){
+            const newTime = time + ((Math.floor(Date.now() / 1000)) - localTime);
+            document.querySelector("#clock").innerText = timeStampToTime(newTime);
+        }        
 
         setInterval(() => {
-            time ++;
             manageServerClock();
         }, 1000);
 
-        document.querySelector("#clock").innerText = timeStampToTime(time);
+        
     </script>
     <p style="color: grey">Serveri kell: <span id="clock"></span> </p>
     <a style="all:unset; cursor: pointer; display: flex; text-transform: capitalize; justify-content: end; align-items: center"  href="{{route('valimised.profile')}}">{{Auth::user()->eesnimi}} {{Auth::user()->perenimi}} &nbsp; @if(Auth::user()->profile_pic != "/assets/logo.png") <img src="{{Auth::user()->profile_pic}}" style="height: 30px; border-radius: 50%; aspect-ratio: 1; object-fit: cover" alt=""> @else <i class="material-icons">person</i> @endif</a>
+
+    <script>document.querySelector("#clock").innerText = timeStampToTime(time);</script>
+
 </div>
