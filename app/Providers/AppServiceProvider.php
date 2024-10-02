44735d18413c4cdb7b4cb5fce150a60f075ccc12
AstrayJ <jarl.justus.hellat@gmail.com>
2024-10-02 21:03:21 +0300

    pulse should work

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::define('viewPulse', function (User $user) {
-            return $user->id == 9 || $user->id == 1000002 || $user->id == 5;
+            return $user->id == 9 || $user->id == 1000002 || $user->id == 5 || $user->id == 1;
         });
     }
 }
