74c930d6a5b0b617f5d32407d51bc6ed502bf3d8
JJ <jarl.justus.hellat@gmail.com>
2024-10-02 14:35:12 +0300

    pulse (not working)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+ 
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('viewPulse', function (User $user) {
+            return $user->id == 9 || $user->id == 1000002 || $user->id == 5;
+        });
     }
 }
