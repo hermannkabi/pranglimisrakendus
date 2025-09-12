aee018e9368f6002cbd7e032f190c2327e5ab699
hermannkabi <hermann.kaebi@gmail.com>
2025-09-12 19:51:11 +0300

    Better fox list UI

--- resources/views/foxlist.blade.php
+++ resources/views/foxlist.blade.php
@@ -13,14 +13,17 @@
             <div class="name-row" status={{$item["chosen_by_email"] == null ? "notchosen" : "chosen"}}>
                 <p>{{$item["fox_name"]}}</p>
                 
-                <div>
-                    <p title="{{ $item["chosen_by_email"] }}" style="color:grey">{{$item["chosen_by_name"] ?? "Pole veel valitud"}}</p>
+                <div style="display: flex; flex-direction: row; justify-content: center; align-items: center; gap: 16px;">
+                    <div>
+                        <p title="{{ $item["chosen_by_email"] }}" style="color:grey">{{$item["chosen_by_name"] ?? "Pole veel valitud"}}</p>
+                    </div>
+                    <form action="{{ route("valimised.foxDelete") }}" method="post">
+                        
+                        @csrf
+                        <input type="hidden" name="id" value="{{ $item["id"] }}">
+                        <button style="padding-inline: 16px" class="red" type="submit"><i style="font-size: 16px; " class="material-icons">delete</i></button>
+                    </form>
                 </div>
-                <form action="{{ route("valimised.foxDelete") }}" method="post">
-                    @csrf
-                    <input type="hidden" name="id" value="{{ $item["id"] }}">
-                    <button style="padding-inline: 16px" class="red" type="submit"><i style="font-size: 16px; " class="material-icons">delete</i></button>
-                </form>
     
             </div>
         @endforeach
